9d9b6b473fe5015af8bd94ef4b154cfa562f2fbd
jofer86 <jofer86@gmail.com>
2019-06-27 13:55:46 -0500

    validate user model with a TDD approach.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, presence: true, length: {minimum: 4, maximum: 20}
+  validates :last_name, presence: true, length: {minimum: 4, maximum: 20}
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid with a first name, last name, email and password"
-  it "is invalid without a first name"
-  it "is invalid with a first name with less than 4 characters"
-  it "is invalid with a first name that has more than 20"  
-  it "is invalid without a last name"
-  it "is invalid with a first name with less than 4 characters"
-  it "is invalid with a first name that has more than 20"
-  it "is invalid without a email adress"
+  it "is valid with a first name, last name, email and password" do
+    user = User.new(
+      first_name: "jorge",
+      last_name: "fernando",
+      email: "jorge@gmail.com",
+      password: "password"
+    )
+    expect(user).to be_valid
+  end
+  
+  it "is invalid without a first name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "is invalid with a first name with less than 4 characters" do
+    user = User.new(first_name: "aaa")
+    user.valid?
+    expect(user.errors[:first_name]).to include("is too short (minimum is 4 characters)")
+  end
+
+  it "is invalid with a first name that has more than 20" do
+    user = User.new(first_name: "a" * 21)
+    user.valid?
+    expect(user.errors[:first_name]).to include("is too long (maximum is 20 characters)")
+  end
+
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  it "is invalid with a last name with less than 4 characters" do
+    user = User.new(last_name: "aaa")
+    user.valid?
+    expect(user.errors[:last_name]).to include("is too short (minimum is 4 characters)")
+  end
+
+  it "is invalid with a last name that has more than 20" do
+    user = User.new(last_name: "a" * 21)
+    user.valid?
+    expect(user.errors[:last_name]).to include("is too long (maximum is 20 characters)")
+  end
+
+  it "is invalid without a email adress" 
   it "is invalid without a password"
   it "is invalid without a matching password confirmation"
   it "is invalid with a duplicate email adress"
