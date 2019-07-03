1e41846cf4bdd96f4a39d31c2291c6686e2e334a
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-07-03 15:46:18 +0100

    add the dependent destroy option to user, post and profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
-  has_many :posts, foreign_key: "author_id"
-  has_many :comments, foreign_key: "commenter_id"
+  has_many :posts, dependent: :destroy, foreign_key: "author_id"
+  has_many :comments, dependent: :destroy ,foreign_key: "commenter_id"
   has_many :commented_posts, through: :comments, class_name: "Post", source: :post
   has_many :likes, foreign_key: "liker_id"
   has_many :liked_posts, through: :likes, class_name: "Post"
   
 
   #friendship association
-  has_many :invitations, foreign_key: :invitee_id
+  has_many :invitations, dependent: :destroy ,foreign_key: :invitee_id
   # retrieve the users to whom a user has sent a friend request
   has_many :pals, through: :invitations, class_name: "User", source: :invited
-  has_many :requests,foreign_key: :invited_id, class_name: "Invitation"
+  has_many :requests, dependent: :destroy ,foreign_key: :invited_id, class_name: "Invitation"
   # retrieve the users from which a user got friend request
   has_many :users, through: :requests, source: :invitee
   
