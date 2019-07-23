030d41a207f1121c866ebb2c47afec6a72dde40b
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-07-23 17:03:09 +0100

    write tests for user sign up

--- spec/features/facebook_spec.rb
+++ spec/features/facebook_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.feature "Facebooks", type: :feature do
   scenario "creation of a new user" do
+    #current_user = User.first
     visit root_path
     click_link "Sign up"
+    expect(page).to have_current_path("/users/sign_up")
+    expect{
+      fill_in "user_first_name", with: "alexander"
+      fill_in "user_last_name", with: "the great"
+      fill_in "user_email", with: "foo@bar.com"
+      fill_in "user_password", with: "123456"
+      fill_in "user_password_confirmation", with: "123456"
+      click_on "Sign up"
+    }.to change(User.all, :size).by(1)
   end
 end
