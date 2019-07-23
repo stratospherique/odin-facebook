ec31dab93de3dce75efbb2709ecbeedfd0c022c2
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-07-23 21:30:44 +0100

    write false positive tests for user sign up

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
+  gem "koala"
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
--- spec/features/facebook_spec.rb
+++ spec/features/facebook_spec.rb
@@ -1,22 +1,58 @@
 require 'rails_helper'
+require 'koala'
 
 RSpec.feature "Facebooks", type: :feature do
-  scenario "creation of a new user" do
-    #current_user = User.first
-    visit root_path
-    click_link "Sign up"
-    expect(page).to have_current_path("/users/sign_up")
-    expect{
-      fill_in "user_first_name", with: "alexander"
-      fill_in "user_last_name", with: "the great"
-      fill_in "user_email", with: "foo@bar.com"
-      fill_in "user_password", with: "123456"
-      fill_in "user_password_confirmation", with: "123456"
-      click_on "Sign up"
-    }.to change(User.all, :size).by(1)
-  end
+  context "User sign up" do
+    scenario "creation of a new user" do
+      #current_user = User.first
+      visit root_path
+      click_link "Sign up"
+      expect(page).to have_current_path("/users/sign_up")
+      expect{
+        fill_in "user_first_name", with: "alexander"
+        fill_in "user_last_name", with: "the great"
+        fill_in "user_email", with: "foo@bar.com"
+        fill_in "user_password", with: "123456"
+        fill_in "user_password_confirmation", with: "123456"
+        click_on "Sign up"
+      }.to change(User.all, :size).by(1)
+    end
+  
+    scenario "User creation failed because of an empty email" do
+      visit root_path
+      click_link "Sign up"
+      expect(page).to have_current_path("/users/sign_up")
+      expect{
+        fill_in "user_first_name", with: "alexander"
+        fill_in "user_last_name", with: "the great"
+        fill_in "user_email", with: ""
+        fill_in "user_password", with: "123456"
+        fill_in "user_password_confirmation", with: "123456"
+        click_on "Sign up"
+      }.to change(User.all, :size).by(0)
+      expect(page).to have_content "error prohibited this user from being saved"
+      expect(page).to have_content "Email can't be blank"
+    end
 
-  describe "Sign in process", type: feature do
+    scenario "User creation failed because of an empty password" do
+      visit root_path
+      click_link "Sign up"
+      expect(page).to have_current_path("/users/sign_up")
+      expect{
+        fill_in "user_first_name", with: "alexander"
+        fill_in "user_last_name", with: "the great"
+        fill_in "user_email", with: "foo@bar.com"
+        fill_in "user_password", with: ""
+        fill_in "user_password_confirmation", with: ""
+        click_on "Sign up"
+      }.to change(User.all, :size).by(0)
+      expect(page).to have_content "errors prohibited this user from being saved"
+      expect(page).to have_content "Password can't be blank"
+    end
+  end
+  
+
+  
     scenario "Login user" do
       User.create(
         first_name: "jorge",
@@ -25,14 +61,34 @@ RSpec.feature "Facebooks", type: :feature do
         password: "password",
         password_confirmation: "password"
       )
-
+      
       visit root_path
       fill_in "user_email", with: "jorge@gmail.com"
       fill_in "user_password", with: "password"
       click_on "Log in"
       
-      expect(page).to have_content 'Signed in successsfully'
+      expect(page).to have_content 'Signed in successfully'
     end
-  end
-
+=begin
+    context "sign in with facebook" do
+      before do
+        @test_users_api=Koala::Facebook::TestUsers.new(:app_id =>"721321031658977" , :secret =>"07a2196c4e9d1367ea0770f1cdb8aa5b")
+        @test_user=@test_users_api.create(false)
+        # creates a user who hasn't installed your app
+      end
+      after do
+        @test_users_api.delete(@test_user["id"])
+      end
+      scenario "login a user with facebook" do
+        visit root_path
+        click_on "Sign in with Facebook"
+        fill_in "email",with: @test_user["email"]
+        fill_in "pass",with: @test_user["password"]
+        click_on "loginbutton"
+        find(:xpath, "//button[@name='__CONFIRM__']").click
+        expect(page).to have_content("Successfully authenticated from Facebook account.")
+      end
+    end
+=end
+    
 end
