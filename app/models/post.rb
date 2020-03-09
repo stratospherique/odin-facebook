5c2e6afc647f1185922f11e27e1ce78c7a8360a2
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2020-03-10 00:25:46 +0100

    changed posts appearance

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,32 @@ class Post < ApplicationRecord
     has_many :likers, through: :likes, class_name: "User"
     validates :title, presence: true, uniqueness: { scope: :author_id }
     validates :body, presence: true
+
+    def nb_likes
+        self.likes.count
+    end
+
+    def nb_comments
+        self.comments.count
+    end
+
+    def since?
+        time_since(self.updated_at)
+    end
+    
+    private 
+    def time_since(post_date)
+        diff = Time.now - post_date
+        case diff
+        when (3600 * 24)..Float::INFINITY
+            "#{(diff / (3600 * 24)).floor} days"
+        when 3599..(3600 * 24 - 1)
+            "#{(diff / 3600).floor} hours"
+        when 60..3600
+            "#{(diff / 60).floor} minutes"
+        when 10..60
+            "#{diff.floor} seconds"
+        else 'Now'
+        end
+    end
 end
